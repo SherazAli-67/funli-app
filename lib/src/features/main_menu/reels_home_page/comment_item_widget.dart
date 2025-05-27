b8c2e3ef34514d1229be1b61d82abacd32b67f28
SherazAli-67 <soomrosheraz054@gmail.com>
2025-05-27 22:21:56 +0500

    Reels Page UI Development
    1. Set comments and Comment by user on comments page

---
+++ lib/src/features/main_menu/reels_home_page/comment_item_widget.dart
@@ -0,0 +1,62 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:funli_app/src/models/comment_model.dart';
+import 'package:funli_app/src/models/user_model.dart';
+import 'package:funli_app/src/services/user_service.dart';
+
+import '../../../res/app_colors.dart';
+import '../../../res/app_icons.dart';
+import '../../../res/app_textstyles.dart';
+import '../../../widgets/post_like_widget.dart';
+
+class CommentItemWidget extends StatelessWidget {
+  const CommentItemWidget({
+    super.key,
+    required AddCommentModel comment,
+    required String reelID
+  }) : _comment = comment, _reelID = reelID;
+  final AddCommentModel _comment;
+  final String _reelID;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      spacing: 10,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        FutureBuilder(future: UserService.getUserByID(userID: _comment.commentBy), builder: (ctx,snapshot){
+          if(snapshot.hasData && snapshot.requireData != null){
+            UserModel user = snapshot.requireData!;
+            return ListTile(
+              contentPadding: EdgeInsets.zero,
+              leading: CircleAvatar(
+                backgroundImage: CachedNetworkImageProvider(AppIcons.icDummyImgUrl),
+              ),
+              title: Text(user.userName, style: AppTextStyles.tileTitleTextStyle,),
+              trailing: IconButton(
+                  onPressed: (){}, icon: SvgPicture.asset(AppIcons.icMore)),
+            );
+          }
+          return ListTile(
+            contentPadding: EdgeInsets.zero,
+            leading: CircleAvatar(
+              backgroundImage: CachedNetworkImageProvider(AppIcons.icDummyImgUrl),
+            ),
+            title: Text("...", style: AppTextStyles.tileTitleTextStyle,),
+            trailing: IconButton(
+                onPressed: (){}, icon: SvgPicture.asset(AppIcons.icMore)),
+          );
+        }),
+        Text(_comment.comment, style: AppTextStyles.commentTextStyle,),
+        Row(
+          spacing: 20,
+          children: [
+            PostLikeWidget(reelID: _reelID,iconColor: Colors.black, icon: AppIcons.icLikeOutlined,),
+            Text("2 days ago", style: AppTextStyles.captionTextStyle.copyWith(color: AppColors.commentTextColor),),
+            TextButton(onPressed: (){}, child: Text("Reply", style: AppTextStyles.captionTextStyle.copyWith(color: AppColors.commentTextColor),))
+          ],
+        )
+      ],
+    );
+  }
+}
--- lib/src/features/main_menu/reels_home_page/comments_page.dart
+++ lib/src/features/main_menu/reels_home_page/comments_page.dart
@@ -1,15 +1,24 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:funli_app/src/features/main_menu/reels_home_page/comment_item_widget.dart';
+import 'package:funli_app/src/models/comment_model.dart';
 import 'package:funli_app/src/res/app_colors.dart';
 import 'package:funli_app/src/res/app_icons.dart';
 import 'package:funli_app/src/res/app_textstyles.dart';
 import 'package:funli_app/src/services/reels_service.dart';
 import 'package:funli_app/src/widgets/post_like_widget.dart';
 
-class CommentsPage extends StatelessWidget{
+class CommentsPage extends StatefulWidget{
   const CommentsPage({super.key, required String reelID}) : _reelID = reelID;
   final String _reelID;
+
+  @override
+  State<CommentsPage> createState() => _CommentsPageState();
+}
+
+class _CommentsPageState extends State<CommentsPage> {
+  final TextEditingController _commentController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -22,7 +31,7 @@ class CommentsPage extends StatelessWidget{
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               StreamBuilder(
-                  stream: ReelsService.getReelCommentCount(reelID: _reelID),
+                  stream: ReelsService.getReelCommentCount(reelID: widget._reelID),
                   builder: (ctx, snapshot) {
                     if(snapshot.hasData && snapshot.requireData > 0){
                       return Text('Comments (${snapshot.requireData})', style: AppTextStyles.headingTextStyle3,);
@@ -38,36 +47,15 @@ class CommentsPage extends StatelessWidget{
             ],
           ),
 
-          StreamBuilder(stream: ReelsService.getReelsComment(reelID: _reelID), builder: (ctx, snapshot){
+          StreamBuilder(stream: ReelsService.getReelsComment(reelID: widget._reelID), builder: (ctx, snapshot){
             if(snapshot.hasData && snapshot.requireData.isNotEmpty){
               return Expanded(child: ListView.builder(
-                  itemCount: 10,
+                  itemCount: snapshot.requireData.length,
                   itemBuilder: (ctx, index){
+                    AddCommentModel comment = snapshot.requireData[index];
                     return Padding(
                       padding: const EdgeInsets.only(bottom: 24.0),
-                      child: Column(
-                        spacing: 10,
-                        children: [
-                          ListTile(
-                            contentPadding: EdgeInsets.zero,
-                            leading: CircleAvatar(
-                              backgroundImage: CachedNetworkImageProvider(AppIcons.icDummyImgUrl),
-                            ),
-                            title: Text("Kristen Watson", style: AppTextStyles.tileTitleTextStyle,),
-                            trailing: IconButton(
-                                onPressed: (){}, icon: SvgPicture.asset(AppIcons.icMore)),
-                          ),
-                          Text("You're looking absolutely amazing. The apparel looks stunning on you and the combo of you guyz ❤️. Looking forward to see more from you guyz.", style: AppTextStyles.commentTextStyle,),
-                          Row(
-                            spacing: 20,
-                            children: [
-                              PostLikeWidget(reelID: _reelID,iconColor: Colors.black, icon: AppIcons.icLikeOutlined,),
-                              Text("2 days ago", style: AppTextStyles.captionTextStyle.copyWith(color: AppColors.commentTextColor),),
-                              TextButton(onPressed: (){}, child: Text("Reply", style: AppTextStyles.captionTextStyle.copyWith(color: AppColors.commentTextColor),))
-                            ],
-                          )
-                        ],
-                      ),
+                      child: CommentItemWidget(comment: comment, reelID: widget._reelID)
                     );
                   }));
             }
@@ -82,6 +70,7 @@ class CommentsPage extends StatelessWidget{
           }),
 
           Container(
+
             decoration: BoxDecoration(
               color: AppColors.commentTextFieldFillColor,
               borderRadius: BorderRadius.circular(13),
@@ -94,6 +83,7 @@ class CommentsPage extends StatelessWidget{
                     children: [
                       IconButton(onPressed: (){}, icon: Icon(Icons.emoji_emotions_outlined)),
                       Expanded(child: TextField(
+                        controller: _commentController,
                         decoration: InputDecoration(
                           border: InputBorder.none,
                           enabledBorder: InputBorder.none,
@@ -105,7 +95,12 @@ class CommentsPage extends StatelessWidget{
                     ],
                   ),
                 ),
-                IconButton(onPressed: (){}, icon: SvgPicture.asset(AppIcons.icSendBtn))
+                IconButton(onPressed: ()async{
+                  String commentText = _commentController.text.trim();
+                  await ReelsService.addCommentToReel(reelID: widget._reelID, commentText: commentText);
+                  FocusManager.instance.primaryFocus?.unfocus();
+                  _commentController.clear();
+                }, icon: SvgPicture.asset(AppIcons.icSendBtn))
               ],
             ),
           )
@@ -113,5 +108,5 @@ class CommentsPage extends StatelessWidget{
       ),
     );
   }
-
 }
+
